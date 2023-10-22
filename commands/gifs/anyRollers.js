8c9f74df8825db5c6230423dc6140e0750d4c433
Beto Quezada <Beetoo1313@gmail.com>
2023-10-22 01:25:28 -0600

    try of command removing spaces

--- commands/gifs/anyRollers.js
+++ commands/gifs/anyRollers.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("any rollers")
+    .setName("AnyRollers")
     .setDescription("Any rollers in the chat"),
   async execute(interaction) {
     await interaction.reply({
