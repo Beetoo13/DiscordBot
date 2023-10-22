5826265bb7deae8dc355ea5b04e70d48503ad08c
Beto Quezada <Beetoo1313@gmail.com>
2023-10-22 01:44:25 -0600

    testing of deploying (/) commands

--- index.js
+++ index.js
@@ -27,6 +27,56 @@ for (const folder of commandFolders) {
   }
 }
 
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+// const foldersPath = path.join(__dirname, "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
+
 // Gifs
 const ratapuke = require("./commands/gifs/Ratapuke");
 const booba = require("./commands/gifs/Booba");
@@ -54,7 +104,7 @@ const agarrameWe = require("./commands/imgs/AgarrameWe");
 // Other stuff
 const prime = require("./commands/otherStuff/Prime");
 const avatar = require("./commands/otherStuff/Avatar");
-const commands = require("./commands/otherStuff/Commands");
+const commands2 = require("./commands/otherStuff/Commands");
 const hola = require("./commands/otherStuff/Hola");
 const dice = require("./commands/otherStuff/Dice");
 const bities = require("./commands/otherStuff/Bities");
@@ -100,7 +150,7 @@ client.on("message", (msg) => {
   // Other Stuff
   prime(msg);
   avatar(msg);
-  commands(msg);
+  commands2(msg);
   hola(msg);
   dice(msg);
   bities(msg);
